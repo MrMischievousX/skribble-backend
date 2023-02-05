66e973cfb2b1fa9be01ac0f73de0e3602595814f
Akash Tureha <turehaakash123@gmail.com>
2023-02-05 17:03:28 +0530

    new logic changes

--- index.js
+++ index.js
@@ -1,92 +1,35 @@
 const http = require("http");
 const webSocketServer = require("websocket").server;
 const httpServer = http.createServer();
+const keyWords = require("./keywords");
 
+//Game Variables
 const clients = {};
 const games = {};
 let idx = 0;
-const keyWords = [
-  "America",
-  "Balloon",
-  "Biscuit",
-  "Blanket",
-  "Chicken",
-  "Chimney",
-  "Country",
-  "Cupcake",
-  "Curtain",
-  "Diamond",
-  "Eyebrow",
-  "Fireman",
-  "Florida",
-  "Germany",
-  "Harpoon",
-  "Husband",
-  "Morning",
-  "Octopus",
-  "Popcorn",
-  "Printer",
-  "Sandbox",
-  "Skyline",
-  "Spinach",
-  "Blueberry",
-  "Breakfast",
-  "Bubblegum",
-  "Cellphone",
-  "Dandelion",
-  "Hairbrush",
-  "Hamburger",
-  "Horsewhip",
-  "Jellyfish",
-  "Landscape",
-  "Nightmare",
-  "Pensioner",
-  "Rectangle",
-  "Snowboard",
-  "Spaceship",
-  "Spongebob",
-  "Swordfish",
-  "Telephone",
-  "Telescope",
-  "Backpack",
-  "Basement",
-  "Building",
-  "Campfire",
-  "Complete",
-  "Elephant",
-  "Exercise",
-  "Hospital",
-  "Internet",
-  "Jalapeno",
-  "Mosquito",
-  "Sandwich",
-  "Scissors",
-  "Seahorse",
-  "Skeleton",
-  "Snowball",
-  "Sunshade",
-  "Treasure",
-];
-let answer = "";
 
-httpServer.listen(process.env.PORT || 5000, () => {
-  console.log(`Listening to ${process.env.PORT || 5000}`);
+//Starting http server
+httpServer.listen(process.env.PORT || 8080, () => {
+  console.log(`Listening to ${process.env.PORT || 8080}`);
 });
 
+//Starting websocket server
 const wsServer = new webSocketServer({
   httpServer: httpServer,
 });
 
-// function uuidv4() {
-//   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-//     (
-//       c ^
-//       (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-//     ).toString(16)
-//   );
-// }
+//Function to generate uuid
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
 
-function randomNum(max) {
+//Generating 3 random numbers
+function randomNum() {
   var n = [];
   for (var i = 0; i < 3; i++) {
     n.push(Math.floor(Math.random() * keyWords.length) + 0);
@@ -94,113 +37,91 @@ function randomNum(max) {
   return n;
 }
 
+//Websocket listener
 wsServer.on("request", (request) => {
   const connection = request.accept(null, request.origin);
 
+  console.log(JSON.stringify(Object.keys(clients)));
+  console.log(JSON.stringify(Object.keys(games)));
+
+  //Open connection handler
   connection.on("open", () => {
     console.log("Opened");
   });
 
+  //Close connection handler
   connection.on("close", () => {
-    // clients = {};
-    // games = {};
-    // idx = 0;
+    console.log("Closed");
   });
 
+  //Message handler
   connection.on("message", (message) => {
     const result = JSON.parse(message.utf8Data);
 
+    //Creating a game lobby
     if (result.method == "create") {
       const clientId = result.clientId;
       const gameId = result.name;
-      games[gameId] = {
-        id: gameId,
-        clients: [],
-      };
 
-      const payLoad = {
-        method: "create",
-        game: games[gameId],
-        id: gameId,
-      };
+      //Checking if game with id already exist
+      if (games[gameId]) {
+        console.log("Game already created");
+        const payLoad = {
+          method: "error",
+          code: 1,
+          error: "Server already exist",
+          message: "Name has already have been taken",
+        };
 
-      const con = clients[clientId].connection;
-      con.send(JSON.stringify(payLoad));
+        const con = clients[clientId].connection;
+        con.send(JSON.stringify(payLoad));
+      } else {
+        games[gameId] = {
+          id: gameId,
+          clients: [],
+          answer: "",
+          timer: 60,
+          answerCOunt: 0,
+        };
+
+        const payLoad = {
+          method: "create",
+          game: games[gameId],
+          id: gameId,
+        };
+
+        const con = clients[clientId].connection;
+        con.send(JSON.stringify(payLoad));
+      }
     }
 
+    //Joining a game lobby
     if (result.method == "join") {
       const clientId = result.clientId;
       const gameId = result.gameId;
       const game = games[gameId];
       const len = game.clients.length;
       if (len >= 10) return;
+
       game.clients.push({
         clientId: clientId,
         name: result.name,
         score: 0,
         canType: true,
       });
+
       const payLoad = {
         method: "join",
         game: game,
         id: gameId,
       };
+
       game.clients.forEach((c) => {
         clients[c.clientId].connection.send(JSON.stringify(payLoad));
       });
     }
 
-    if (result.method == "update") {
-      const color = result.color;
-      const offset = result.offset;
-      const clientId = result.clientId;
-      const gameId = result.gameId;
-      const game = games[gameId];
-
-      const payLoad = {
-        color: color,
-        offset: offset,
-        method: "update",
-      };
-
-      game.clients.forEach((c) => {
-        if (c.clientId != clientId)
-          clients[c.clientId].connection.send(JSON.stringify(payLoad));
-      });
-    }
-
-    if (result.method == "startPoint") {
-      const offset = result.offset;
-      const clientId = result.clientId;
-      const gameId = result.gameId;
-      const game = games[gameId];
-
-      const payLoad = {
-        offset: offset,
-        method: "startPoint",
-      };
-
-      game.clients.forEach((c) => {
-        if (c.clientId != clientId)
-          clients[c.clientId].connection.send(JSON.stringify(payLoad));
-      });
-    }
-
-    if (result.method == "closePoint") {
-      const clientId = result.clientId;
-      const gameId = result.gameId;
-      const game = games[gameId];
-
-      const payLoad = {
-        method: "closePoint",
-      };
-
-      game.clients.forEach((c) => {
-        if (c.clientId != clientId)
-          clients[c.clientId].connection.send(JSON.stringify(payLoad));
-      });
-    }
-
+    //Starting a game
     if (result.method == "start") {
       const gameId = result.gameId;
       const game = games[gameId];
@@ -228,6 +149,65 @@ wsServer.on("request", (request) => {
           clients[c.clientId].connection.send(JSON.stringify(allotPayLoad));
         });
       }, 1000);
+
+      setTimeout(() => {
+        cleanGame(gameId);
+      }, 60 * 1000);
+    }
+
+    //Game Logics
+    //Start drawing
+    if (result.method == "startPoint") {
+      const offset = result.offset;
+      const clientId = result.clientId;
+      const gameId = result.gameId;
+      const game = games[gameId];
+
+      const payLoad = {
+        offset: offset,
+        method: "startPoint",
+      };
+
+      game.clients.forEach((c) => {
+        if (c.clientId != clientId)
+          clients[c.clientId].connection.send(JSON.stringify(payLoad));
+      });
+    }
+
+    //Updating drawing
+    if (result.method == "update") {
+      const color = result.color;
+      const offset = result.offset;
+      const clientId = result.clientId;
+      const gameId = result.gameId;
+      const game = games[gameId];
+
+      const payLoad = {
+        color: color,
+        offset: offset,
+        method: "update",
+      };
+
+      game.clients.forEach((c) => {
+        if (c.clientId != clientId)
+          clients[c.clientId].connection.send(JSON.stringify(payLoad));
+      });
+    }
+
+    //Ending drawing
+    if (result.method == "closePoint") {
+      const clientId = result.clientId;
+      const gameId = result.gameId;
+      const game = games[gameId];
+
+      const payLoad = {
+        method: "closePoint",
+      };
+
+      game.clients.forEach((c) => {
+        if (c.clientId != clientId)
+          clients[c.clientId].connection.send(JSON.stringify(payLoad));
+      });
     }
 
     //Validate answer
@@ -236,15 +216,16 @@ wsServer.on("request", (request) => {
       const gameId = result.gameId;
       const game = games[gameId];
 
-      if (answer == result.answer) {
-        game.clients.map((item, index) => {
+      if (game.answer == result.answer) {
+        game.clients.forEach((item) => {
           if (clientId == item.clientId && item.canType) {
             const newItem = item;
             newItem.score += 100;
             newItem.canType = false;
-            return newItem;
-          } else return item;
+          }
         });
+
+        game.clients[idx].score += 25;
       }
 
       const payLoad = {
@@ -259,7 +240,8 @@ wsServer.on("request", (request) => {
 
     //Change answer
     if (result.method == "changeAnswer") {
-      answer = result.answer;
+      const gameId = result.gameId;
+      games[gameId].answer = result.answer;
     }
 
     //Ended drawing
@@ -283,15 +265,15 @@ wsServer.on("request", (request) => {
         clientId: game.clients[idx].clientId,
       };
 
-      setTimeout(() => {
-        game.clients.forEach((c, index) => {
-          clients[c.clientId].connection.send(JSON.stringify(allotPayLoad));
-        });
-      }, 1000);
+      game.clients.forEach((c, index) => {
+        clients[c.clientId].connection.send(JSON.stringify(allotPayLoad));
+      });
     }
   });
 
-  const clientId = Date.now();
+  // Generating unique ids
+  const clientId = uuidv4();
+  // const clientId = Date.now();
   clients[clientId] = { connection: connection };
 
   const payLoad = {
@@ -301,3 +283,32 @@ wsServer.on("request", (request) => {
 
   connection.send(JSON.stringify(payLoad));
 });
+
+const cleanGame = (gameId) => {
+  console.log("GAME CLEANUP");
+
+  const game = games[gameId];
+  const timeOutPayload = {
+    method: "error",
+    code: 2,
+    error: "Server had timeout",
+    message: "Please restart the game",
+  };
+
+  game.clients.forEach((item) => {
+    const client = item.clientId;
+    const con = clients[client].connection;
+
+    con.send(JSON.stringify(timeOutPayload));
+
+    setTimeout(() => {
+      delete clients[client];
+    }, 200);
+  });
+
+  setTimeout(() => {
+    delete games[gameId];
+    console.log(games);
+    console.log(clients);
+  }, 200);
+};
